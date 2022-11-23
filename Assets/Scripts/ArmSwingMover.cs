540622efbe0b7ca2fe2379c692d5f740fdde940e
Chen, Chang-Min <47084071+chenchami@users.noreply.github.com>
2022-11-23 21:58:08 +0800

    stabling the arm swing checking

--- Assets/Scripts/ArmSwingMover.cs
+++ Assets/Scripts/ArmSwingMover.cs
@@ -3,84 +3,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class ArmSwingMover : MonoBehaviour
 {
     public OVRHand LeftHand;
     public OVRHand RightHand;
     public OVRCameraRig CameraRig;
-
-    // Vector3 positions
-    private Vector3 _previousPositionLeft;
-    private Vector3 _previousPositionRight;
-    private Vector3 _currentPositionLeft;
-    private Vector3 _currentPositionRight;
-
-    // Speed
-    [SerializeField] private float speed;
-    [SerializeField] private float handSpeedThreshold;
-    [SerializeField] private float dragForce;
-    private float _handSpeed;
     private Rigidbody _rigidbody;
     
-
+    private Vector3 _previousLocalPositionLeft;
+    private Vector3 _previousLocalPositionRight;
+    private Vector3 _currentLocalPositionLeft;
+    private Vector3 _currentLocalPositionRight;
+    
+    [SerializeField] private float speed;
+    [SerializeField] private float handMovingThreshold;
+    [SerializeField] private float dragForce;
+   
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         
         // Set original PreviousFrame positions at start up
-        _previousPositionLeft = LeftHand.PointerPose.position;
-        _previousPositionRight = RightHand.PointerPose.position;
+        _previousLocalPositionLeft = LeftHand.PointerPose.position;
+        _previousLocalPositionRight = RightHand.PointerPose.position;
     }
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ReloadLevel();
-        }
+        if (Input.GetKeyDown(KeyCode.R)) ReloadLevel();
     }
 
     private void FixedUpdate()
     {
-        // Get current position of the camera, left and right hands
-        _currentPositionLeft = LeftHand.PointerPose.position;
-        _currentPositionRight = RightHand.PointerPose.position;
-
-        // Get distance the hands and player has moved since the last frame
-        var leftHandVectorMoved = _currentPositionLeft - _previousPositionLeft;
-        var rightHandVectorMoved = _currentPositionRight - _previousPositionRight;
-        
-        // Add them up to get the handSpeed from the user minus the movement of the player to neglect the movement of the player from the
-        _handSpeed = Mathf.Abs(leftHandVectorMoved.y) + Mathf.Abs(rightHandVectorMoved.y);
-
-        // Set previous position of the camera, left and right hands
-        _previousPositionLeft = _currentPositionLeft;
-        _previousPositionRight = _currentPositionRight;
-
         if (Time.timeSinceLevelLoad < 1f) return;
         
+        // Get current position of the camera, left and right hands
+        _currentLocalPositionLeft = LeftHand.PointerPose.position;
+        _currentLocalPositionRight = RightHand.PointerPose.position;
 
-        if (_rigidbody.velocity.sqrMagnitude > 0.01f)
-        {
-            DragMovement();
-        }
-        
-        // Move the player
-        var moveDir = GetForwardDirection();
-        if (_handSpeed > handSpeedThreshold)
-        {
-            _rigidbody.velocity = speed * moveDir;
-        }
+        // Handle arm swinging movement    
+        if (IsDragging()) DragMovement();
+        if (IsArmSwinging()) MovePlayer();
 
-
+        // Set previous position of the camera, left and right hands
+        _previousLocalPositionLeft = _currentLocalPositionLeft;
+        _previousLocalPositionRight = _currentLocalPositionRight;
     }
 
-    private Vector3 GetForwardDirection()
+    private bool IsDragging()
     {
-        Vector3 cameraRight = CameraRig.centerEyeAnchor.right;
-        Vector3 playerUpward = transform.up;
-        return Vector3.Cross(cameraRight, playerUpward);
+        return _rigidbody.velocity.sqrMagnitude > 0f;
     }
     private void DragMovement()
     {
@@ -88,11 +62,35 @@ public class ArmSwingMover : MonoBehaviour
         _rigidbody.AddForce(dragForce * dragDir, ForceMode.Acceleration);
     }
 
+    private bool IsArmSwinging()
+    {
+        Vector3 localPositionMovedLeft = _currentLocalPositionLeft - _previousLocalPositionLeft;
+        Vector3 localPositionMovedRight = _currentLocalPositionRight - _previousLocalPositionRight;
+        
+        // Magnitude Check
+        bool isLeftHandMoving = Mathf.Abs(localPositionMovedLeft.y) > handMovingThreshold;
+        bool isRightHandMoving = Mathf.Abs(localPositionMovedRight.y) > handMovingThreshold;
+        
+        // Direction Check
+        bool isReverseDirection = localPositionMovedLeft.y * localPositionMovedRight.y < 0f;
+
+        return  isLeftHandMoving & isRightHandMoving & isReverseDirection;
+    }
+    
+    private void MovePlayer()
+    {
+        // Get the forward direction bases on the direction the player is looking at
+        Vector3 cameraRight = CameraRig.centerEyeAnchor.right;
+        Vector3 playerUpward = transform.up;
+        Vector3 moveDir = Vector3.Cross(cameraRight, playerUpward);
+        
+        // Move player by setting the constant speed
+        _rigidbody.velocity = speed * moveDir;
+    }
 
     private void ReloadLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
     }
-    
 }
