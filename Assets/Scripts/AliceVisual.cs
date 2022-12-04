850d287308fe333aaf911e21bc56dda3aa05335f
Chen, Chang-Min <47084071+chenchami@users.noreply.github.com>
2022-12-04 23:16:39 +0800

    add anime switch to Queen & Alice

--- Assets/Scripts/AliceVisual.cs
+++ Assets/Scripts/AliceVisual.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AliceVisual : MonoBehaviour
@@ -8,19 +9,58 @@ public class AliceVisual : MonoBehaviour
     public OVRCameraRig CameraRig;
     private Transform _transformReference;
     private Vector3 _localPositionOffset;
+    private Vector3 _localRotationOffset;
+
+    private Transform _selfTransform;
+    private Rigidbody _rigidbody;
+    [SerializeField] private Animator _animator;
+    private int _isWalkingHash;
 
     private void Awake()
     {
-        _localPositionOffset = transform.localPosition;
+        _selfTransform = transform;
+        _localPositionOffset = _selfTransform.localPosition;
+        _localRotationOffset = _selfTransform.localRotation.eulerAngles;
         _transformReference = CameraRig.centerEyeAnchor.transform;
+    }
+
+    void Start()
+    {
+        _rigidbody = transform.root.GetComponent<Rigidbody>();
+        _isWalkingHash = Animator.StringToHash("isWalking");
     }
 
     void Update()
     {
+        // Local position & rotation follow camera 
         Vector3 localRotationRef = _transformReference.localRotation.eulerAngles;
-        transform.localRotation = Quaternion.Euler(0f, localRotationRef.y, 0f);
-
+        _selfTransform.localRotation = Quaternion.Euler(0f + _localRotationOffset.x, localRotationRef.y + _localRotationOffset.y, 0f + _localRotationOffset.z);
         Vector3 localPositionRef = _transformReference.localPosition;
-        transform.localPosition = localPositionRef + _localPositionOffset;
+        _selfTransform.localPosition = localPositionRef + _localPositionOffset;
+        
+        // Control animations
+        bool isWalking = _animator.GetBool(_isWalkingHash);
+        
+        if (!isWalking && CheckIsWalking())
+        {
+            _animator.SetBool(_isWalkingHash, true);
+        }
+
+        if (isWalking && !CheckIsWalking())
+        {
+            _animator.SetBool(_isWalkingHash, false);
+        }
+    }
+    
+    private bool CheckIsWalking()
+    {
+        bool result = false;
+        const float threshold = 0.1f; 
+        Vector3 velocity = _rigidbody.velocity;
+        
+        if (velocity.sqrMagnitude > threshold) {
+            result = true;
+        }
+        return result;
     }
 }
--- Assets/Scripts/QueenHandler.cs
+++ Assets/Scripts/QueenHandler.cs
@@ -1,31 +1,54 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.AI;
 
 public class QueenHandler : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent _navMeshAgent;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
 
-    [SerializeField] private Transform _player;
-    
+    [SerializeField] private Transform _target;
+    private int _isRunningHash;
 
-    private void Awake()
+    void Start()
     {
-        _player = GameObject.FindWithTag("Player").transform;
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _isRunningHash = Animator.StringToHash("isRunning");
     }
     
     void Update()
     {
-        ChasePlayer();
-    }
+        // The target who the queen will chase
+        _navMeshAgent.SetDestination(_target.position);
+        
+        // Control the animations
+        bool isRunning = _animator.GetBool(_isRunningHash);
+        
+        if (!isRunning && CheckIsRunning())
+        {
+            _animator.SetBool(_isRunningHash, true);
+        }
 
-    private void ChasePlayer()
-    {
-        _navMeshAgent.SetDestination(_player.position);
+        if (isRunning && !CheckIsRunning())
+        {
+            _animator.SetBool(_isRunningHash, false);
+        }
     }
     
+
+    private bool CheckIsRunning()
+    {
+        bool result = false;
+        Vector3 velocity = _navMeshAgent.velocity;
+        
+        if (velocity.sqrMagnitude > Mathf.Epsilon) {
+            result = true;
+        }
+        return result;
+    }
 }
