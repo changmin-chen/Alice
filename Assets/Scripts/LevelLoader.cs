f8e0e8b4b56aab2666efecb609a8bdd40eae5a53
Chen, Chang-Min <47084071+chenchami@users.noreply.github.com>
2022-12-09 05:49:15 +0800

    scene transition; clean asset

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Animator _transition;
+    private float transitionTime = 1f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        StartCoroutine(LoadLevel(nextSceneIndex));
+    }
+
+    IEnumerator LoadLevel(int sceneIndex)
+    {
+        _transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
