5f03a2e9bbb873c182ff1abcec3f349ba28db724
Chen, Chang-Min <47084071+chenchami@users.noreply.github.com>
2022-12-09 20:05:23 +0800

    rev endgame visual

--- Assets/Scripts/EndgameTrigger.cs
+++ Assets/Scripts/EndgameTrigger.cs
@@ -6,12 +6,41 @@ using UnityEngine;
 public class EndgameTrigger : MonoBehaviour
 {
     [SerializeField] private LevelLoader _levelLoader;
+    [SerializeField] private GameObject _player;
 
+    private Rigidbody _rigidbody;
+    private ArmSwingMover _armSwingMover;
+
+    [SerializeField] private float _fallingDownSpeed;
+    [SerializeField] private float _fallingDownSeconds;
+
+    private void Start()
+    {
+        _armSwingMover = _player.GetComponent<ArmSwingMover>();
+        _rigidbody = _player.GetComponent<Rigidbody>();
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _levelLoader.LoadNextLevel();
+            StartCoroutine(EndgameSequence());
         }
+    }
+
+    IEnumerator EndgameSequence()
+    {
+        // Disable movement
+        _armSwingMover.enabled = false;
+        
+        // Drop down by constant velocity
+        _rigidbody.useGravity = false;
+        _rigidbody.velocity = _fallingDownSpeed * Vector3.down;
+        
+        // Wait for seconds
+        yield return new WaitForSeconds(_fallingDownSeconds);
+
+        // Load next level
+        _levelLoader.LoadNextLevel();
     }
 }
